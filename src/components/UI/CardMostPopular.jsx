52f06909c10e97ea4f279ab842f5190ac6efcf33
EpicZuko <zukokalilov5@gmail.com>
2025-02-22 18:22:52 +0600

    card most popular

---
+++ src/components/UI/CardMostPopular.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel'
+import styled from 'styled-components'
+import 'react-multi-carousel/lib/styles.css'
+import { CartdAdsAutoPlay, popularCard } from '../../utils/constants/URL'
+
+const Wrapper = styled.div`
+  max-width: 1320px;
+  margin: auto;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 450px) {
+    padding: 0px 0px 0px 50px;
+  }
+  .react-multiple-carousel__arrow {
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 0px 0px 4px 0px rgba(0, 145, 66, 0.1);
+    border: none;
+    color: rgba(0, 145, 66, 1) !important;
+    font-size: 30px;
+  }
+  .react-multiple-carousel__arrow::before {
+    color: rgba(0, 145, 66, 1) !important;
+  }
+  .react-multiple-carousel__arrow--left {
+    left: 4px;
+    &:hover {
+      background: rgba(0, 111, 51, 1);
+    }
+    @media (max-width: 430px) {
+      display: none;
+    }
+  }
+  .react-multiple-carousel__arrow--right {
+    right: 22px;
+    &:hover {
+      background: rgba(0, 111, 51, 1);
+    }
+    @media (max-width: 430px) {
+      display: none;
+    }
+  }
+`
+const DIVMOSTPOPULAR = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 38px;
+  background-color: rgba(247, 247, 247, 1);
+  width: 424px;
+  height: 187px;
+  border-radius: 30px;
+  @media (max-width: 450px) {
+    width: 297px;
+    height: 127px;
+    border-radius: 20.31px;
+  }
+`
+const PopularImages = styled.img`
+  width: 154px;
+  height: 149px;
+  @media (max-width: 450px) {
+    width: 104px;
+    height: 100px;
+  }
+`
+const PopularH1 = styled.h1`
+  color: rgba(54, 54, 54, 1);
+  font-family: Nunito Sans;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33.6px;
+  @media (max-width: 450px) {
+    font-size: 16px;
+    line-height: 22.4px;
+  }
+`
+const PopularSpan = styled.span`
+  font-family: Nunito Sans;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+  color: rgba(127, 127, 127, 1);
+  @media (max-width: 450px) {
+    font-size: 14px;
+    line-height: 19.6px;
+  }
+`
+const CardMostPopular = ({ dataArray }) => {
+  return (
+    <Wrapper>
+      <Carousel
+        responsive={popularCard}
+        autoPlay={CartdAdsAutoPlay.autoPlay}
+        autoPlaySpeed={CartdAdsAutoPlay.autoPlaySpeed}
+        infinite={CartdAdsAutoPlay.infinite}
+      >
+        {dataArray?.map((element) => (
+          <DIVMOSTPOPULAR>
+            <div>
+              <PopularImages src={element.images} alt='images' />
+            </div>
+            <div>
+              <PopularH1>{element.name}</PopularH1>
+              <PopularSpan>от {element.price} сом</PopularSpan>
+            </div>
+          </DIVMOSTPOPULAR>
+        ))}
+      </Carousel>
+    </Wrapper>
+  )
+}
+
+export default CardMostPopular
--- src/utils/constants/URL.js
+++ src/utils/constants/URL.js
@@ -47,3 +47,26 @@ export const storiesCard = {
     items: 1,
   },
 }
+
+export const popularCard = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 3,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 2,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 1,
+  },
+  mobileLarge: {
+    breakpoint: { max: 428, min: 320 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 320, min: 0 },
+    items: 1,
+  },
+}
